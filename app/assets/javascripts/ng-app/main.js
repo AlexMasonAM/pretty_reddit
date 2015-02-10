bb2aeb5c5d6883fc7a351b34e36101899848815d
AlexMasonAM <alexmmason@gmail.com>
2015-02-10 11:07:30 -0800

    added angular routes

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -1,10 +1,44 @@
 angular
-  .module("PrettyReddit", [])
+  .module("PrettyReddit", ['ngResource'])
+  .config(function($httpProvider) {
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] =
+    $('meta[name=csrf-token]').attr('content')
+  })
   .controller("postsController", postsController);
 
-  function postsController($scope, $http) {
-    $http.get('api/posts').success(
-      function(data){
-        $scope.posts = data;
-      });
+  function postsController($scope, $http, $resource) {
+    
+    var Post = $resource('/api/posts/:id', {id: '@id'},
+      {
+        'update': { method: 'patch' }
+      }
+    );
+
+    // maps query to the index method
+    $scope.posts = Post.query();
+    
+    // works the same way as rails restful SHOW
+    $scope.post = Post.get({id: 1});
+
+    new Post({
+      title: 'Gardens of the Moon',
+      link: 'http://en.wikipedia.org/wiki/Gardens_of_the_Moon'
+    }).$save();
+
+    Post.get({id: 1}, function(post) {
+      post.title = "Funner stiff";
+      post.$update();
+    });
+
+  $scope.destroyPost = function(post){
+    $scope.post = Post.get({id: post.id}, function(post) {
+    post.$delete();
+    $scope.posts = Post.query();
+    });
+  }
+
+    // $http.get('api/posts').success(
+    //   function(data){
+    //     $scope.posts = data;
+    //   });
   }
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,14 +1,57 @@
 class Api::PostsController < ApplicationController
+  # protect_from_forgery with: :null_session ((bad practice to include during production))
   def index
     posts = Post.all
     render json: posts, 
-           except: [:updated_at, :created_at], 
-           include: {
-                      comments: { except: [:updated_at, :user_id, :post_id]}
-                      # users:    { except: [:updated_at, :password_digest]}
-                    }
+                 except: [:updated_at, :created_at], 
+                 include: {
+                            comments: { except: [:updated_at, :user_id, :post_id]}
+                            # users:    { except: [:updated_at, :password_digest]}
+                          }
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post,
+                 except: [:updated_at, :created_at], 
+                 include: {
+                            comments: { except: [:updated_at, :user_id, :post_id]}
+                            # users:    { except: [:updated_at, :password_digest]}
+                          }
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: {errors: post.errors}, status: 422
+    end
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      render json: post
+    else
+      render json: {errors: post.errors}, status: 422
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy
+      render json: post, status: 200
+    else
+      render json: post, status: 400
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:id, :title, :link, :user_id)
   end
 end
+
 
 # OR USE THE MODULE SYNTAX
 # module Api
